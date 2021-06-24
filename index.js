e55f4cb46ebd6efc572114e1562c67de67de5193
WaseemAhmad790 <86422247+WaseemAhmad790@users.noreply.github.com>
2021-06-24 21:12:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+var slideIndex = 0;
+
+showSlide();
+
+var timer = setInterval(showSlide, 2000);
+
+var slideContainer = document.getElementById('slideshow-container');
+var slideControl = document.getElementById('slide-control');
+
+slideContainer.onmouseover = function() {
+	clearInterval(timer);
+}
+
+slideControl.onmouseover = function() {
+	clearInterval(timer);
+}
+
+slideContainer.onmouseleave = function() {
+	timer = setInterval(showSlide, 2000);
+}
+
+slideControl.onmouseleave = function() {
+	timer = setInterval(showSlide, 2000);
+}
+
+function showSlide() {
+	var i;
+	var slides = document.getElementsByClassName('slide-item');
+	var dots = document.getElementsByClassName('dot');
+	for (var i = 0; i < slides.length; i++) {
+		slides[i].style.display = 'none';
+	}
+	for (var i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" active", "");
+	}
+	slideIndex++;
+	if (slideIndex > slides.length) slideIndex = 1;
+	slides[slideIndex-1].style.display = "block";
+	dots[slideIndex-1].className += " active";
+	autoplay=true;
+}
+
+function chooseSlide(n) {
+	slideIndex = n;
+	showSlide();
+	clearInterval(timer);
+	autoplay=true;
+}
+
+function showMenu() {
+	var topNav = document.getElementById('topnav');
+	if (topNav.className === "navbar") {
+		topNav.className += " show";
+	} else {
+		topNav.className = "navbar";
+	}
+}
